8f99df6d7dffe19c001f3818cdb10a1a541ed009
Tuuz <silverhawk@foxmail.com>
2021-11-25 17:16:54 +0800

    TuuzMac

--- lib/app/index1/scanner/scanner.dart
+++ lib/app/index1/scanner/scanner.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:scan/scan.dart';
 
 ///
 /// FullScreenScannerPage
@@ -8,14 +10,95 @@ class Scanner extends StatefulWidget {
 }
 
 class _Scanner extends State<Scanner> {
+  String _platformVersion = 'Unknown';
+
+  ScanController controller = ScanController();
+  String qrcode = 'Unknown';
+
   @override
-  // TODO: implement context
-  BuildContext get context => super.context;
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    try {
+      platformVersion = await Scan.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: [
+            Text('Running on: $_platformVersion\n'),
+            Wrap(
+              children: [
+                ElevatedButton(
+                  child: Text("toggleTorchMode"),
+                  onPressed: () {
+                    controller.toggleTorchMode();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("pause"),
+                  onPressed: () {
+                    controller.pause();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("resume"),
+                  onPressed: () {
+                    controller.resume();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("parse from image"),
+                  onPressed: () async {
+                    List<Media>? res = await ImagesPicker.pick();
+                    if (res != null) {
+                      String? str = await Scan.parse(res[0].path);
+                      if (str != null) {
+                        setState(() {
+                          qrcode = str;
+                        });
+                      }
+                    }
+                  },
+                ),
+              ],
+            ),
+            Container(
+              width: 220,
+              height: 400,
+              child: ScanView(
+                controller: controller,
+                scanAreaScale: .7,
+                scanLineColor: Colors.green,
+                onCapture: (data) {
+                  setState(() {
+                    qrcode = data;
+                  });
+                },
+              ),
+            ),
+            Text('scan result is $qrcode'),
+          ],
+        ),
+      ),
+    );
   }
-
 }
