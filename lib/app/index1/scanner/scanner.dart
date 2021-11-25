c25569e4c9a6e1467d3d6132a8a88ce33be83d18
Tuuz <silverhawk@foxmail.com>
2021-11-25 15:45:10 +0800

    TuuzMac

--- lib/app/index1/scanner/scanner.dart
+++ lib/app/index1/scanner/scanner.dart
@@ -10,6 +10,15 @@ class _Scanner extends State<Scanner> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        title: Text("扫码"),
+        backgroundColor: Color.fromRGBO(0, 0, 0, 0.5),
+      ),
+      body: Center(
+
+      ),
+    );
   }
 }
